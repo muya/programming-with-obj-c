140b1ad319df30f0d277a8bf76dacb0e2ebdf1db
Muya <kingkonig@gmail.com>
2014-07-13 18:39:16 +0300

    initial commit

---
+++ ProgrammingWithObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ProgrammingWithObjectiveC
+//
+//  Created by Muya on 7/13/14.
+//  Copyright (c) 2014 muya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
